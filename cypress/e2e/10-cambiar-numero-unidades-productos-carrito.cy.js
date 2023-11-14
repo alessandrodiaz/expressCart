6e3cb62a6bcd9334524c77d6f0ddf8609b0a67e3
alessandrodiaz <alessdg02@gmail.com>
2023-11-13 21:23:20 -0500

    Correccion prueba historia 10

--- cypress/e2e/10-cambiar-numero-unidades-productos-carrito.cy.js
+++ cypress/e2e/10-cambiar-numero-unidades-productos-carrito.cy.js
@@ -1,168 +1,167 @@
 /**
  * HISTORIA DE USUARIO #1
  * AGREGAR PRODUCTOS AL CARRITO
- * -> USANDO LOS BOTONES DE '-' y '+'
  * PRUEBAS FUNCIONALES
  * */
 
 describe("Prueba de valor debajo del límite inferior", () => {
-	it("Cuando el valor esté en 1 y se hace clic en el botón '-', el valor debe seguir siendo 1, al oprimir el botón 'ADD TO CART' mostrar mensaje exitoso", () => {
-		// Visita la página que contiene el elemento de entrada de cantidad
+	it("Agregando un producto, cuando el valor esté en 1 y se hace clic en el botón '-', el valor debe seguir siendo 1", () => {
 		cy.visit("http://localhost:1111/product/nike-revolution-6");
 
-		// Encuentra el elemento de entrada de cantidad por su ID
-		cy.get("#product_quantity").as("inputQuantity");
+		// Oprimir botón 'Add to cart' para agregar un producto
+		cy.get(".product-add-to-cart").click();
 
-		// Verifica que el valor inicial sea 1
+		// Abrir carrito
+		cy.contains("Cart").click();
+
+		cy.wait(500);
+
+		// Obtener campo de unidades del producto
+		cy.get(".cart-product-quantity").as("inputQuantity");
+
+		// Verifica que el valor sea 1
 		cy.get("@inputQuantity").should("have.value", "1");
 
 		// Hacer clic en el botón "-" para disminuir el valor
-		cy.get("@inputQuantity").prev(".input-group-prepend").find(".qty-btn-minus").click();
-
-		// Oprimir botón 'Add to cart'
-		cy.get(".product-add-to-cart").click();
+		cy.get("@inputQuantity").prev(".input-group-prepend").find(".btn-qty-minus").click();
 
 		// Verifica que el valor siga siendo 1
 		cy.get("@inputQuantity").should("have.value", "1");
-
-		cy.wait(1000);
-		// cy.get("#notify_message").should("be.visible").and("contain", "Cart successfully updated");
-		cy.get("#notify_message")
-			.should("be.visible") // Esperar a que el mensaje se muestre
-			.should("contain", "Cart successfully updated"); // Verificar que el mensaje contenga el texto deseado
-		// .should("not.be.visible") // Esperar a que el mensaje desaparezca
-		// .should("not.exist"); // Verificar que el mensaje ya no existe en el DOM
 	});
 });
 
 describe("Prueba de valor en el límite inferior", () => {
-	it("El valor debe estar en 1 al ingresar a la página, al oprimir el botón ADD TO CART mostrar mensaje exitoso", () => {
-		// Visita la página que contiene el elemento de entrada de cantidad
+	it("Agregando un producto, el valor debe estar en 1 al ingresar al carrito", () => {
 		cy.visit("http://localhost:1111/product/nike-revolution-6");
 
-		// Encuentra el elemento de entrada de cantidad por su ID
-		cy.get("#product_quantity").as("inputQuantity");
-
-		// Verifica que el valor inicial sea 1
-		cy.get("@inputQuantity").should("have.value", "1");
-
+		// Oprimir botón 'Add to cart' para agregar un producto
 		cy.get(".product-add-to-cart").click();
 
-		cy.get("#notify_message")
-			.should("be.visible") // Esperar a que el mensaje se muestre
-			.should("contain", "Cart successfully updated"); // Verificar que el mensaje contenga el texto deseado
+		// Abrir carrito
+		cy.contains("Cart").click();
+
+		cy.wait(500);
+
+		// Obtener campo de unidades del producto
+		cy.get(".cart-product-quantity").as("inputQuantity");
+
+		// Verifica que el valor sea 1
+		cy.get("@inputQuantity").should("have.value", "1");
 	});
 });
 
 describe("Prueba de valor encima del límite inferior", () => {
-	it("El valor debe estar en 2 oprimiendo una vez el botón '-', al oprimir el botón ADD TO CART mostrar mensaje exitoso", () => {
-		// Visita la página que contiene el elemento de entrada de cantidad
+	it("Agregando un producto, el valor debe estar en 2 oprimiendo una vez el botón '+'", () => {
 		cy.visit("http://localhost:1111/product/nike-revolution-6");
 
-		// Encuentra el elemento de entrada de cantidad por su ID
-		cy.get("#product_quantity").as("inputQuantity");
-
-		// Verifica que el valor inicial sea 1
-		cy.get("@inputQuantity").should("have.value", "1");
-
-		// Presionar una vez el boton
-		cy.get("@inputQuantity").next(".input-group-append").find(".qty-btn-plus").click();
-
-		// Verificar que la cantidad sean 2 unidades
-		cy.get("@inputQuantity").should("have.value", "2");
-
-		// Presionar botón 'Add to cart'
+		// Oprimir botón 'Add to cart' para agregar un producto
 		cy.get(".product-add-to-cart").click();
 
-		cy.get("#notify_message")
-			.should("be.visible") // Esperar a que el mensaje se muestre
-			.should("contain", "Cart successfully updated"); // Verificar que el mensaje contenga el texto deseado
+		// Abrir carrito
+		cy.contains("Cart").click();
+
+		cy.wait(500);
+
+		// Obtener campo de unidades del producto
+		cy.get(".cart-product-quantity").as("inputQuantity");
+
+		// Verifica que el valor sea 1
+		cy.get("@inputQuantity").should("have.value", "1");
+
+		// Hacer clic en el botón "+"
+		cy.get(".btn-qty-add").click();
+
+		// Verifica que el valor sea 2
+		cy.get("@inputQuantity").should("have.value", "2");
 	});
 });
 
 describe("Prueba de valor debajo del límite superior", () => {
-	it("El valor debe estar en 24 oprimiendo el botón '-' 23 veces, al oprimir el botón ADD TO CART mostrar mensaje exitoso", () => {
-		// Visita la página que contiene el elemento de entrada de cantidad
+	it("Agregando un producto, el valor debe estar en 24 oprimiendo el botón '-' 23 veces", () => {
 		cy.visit("http://localhost:1111/product/nike-revolution-6");
 
-		// Encuentra el elemento de entrada de cantidad por su ID
-		cy.get("#product_quantity").as("inputQuantity");
+		// Oprimir botón 'Add to cart' para agregar un producto
+		cy.get(".product-add-to-cart").click();
 
-		// Verifica que el valor inicial sea 1
+		// Abrir carrito
+		cy.contains("Cart").click();
+
+		cy.wait(500);
+
+		// Obtener campo de unidades del producto
+		cy.get(".cart-product-quantity").as("inputQuantity");
+
+		// Verifica que el valor sea 1
 		cy.get("@inputQuantity").should("have.value", "1");
 
 		// Hacer clic en el botón de aumento (incremento) de cantidad 24 veces
 		for (let i = 0; i < 23; i++) {
-			cy.get("@inputQuantity").next(".input-group-append").find(".qty-btn-plus").click();
+			cy.get(".btn-qty-add").click();
 		}
 
-		// Verificar que la cantidad sean 24 unidades
+		cy.wait(500);
+
+		// Verifica que el valor sea 2
 		cy.get("@inputQuantity").should("have.value", "24");
-
-		// Presionar botón 'Add to cart'
-		cy.get(".product-add-to-cart").click();
-
-		cy.get("#notify_message")
-			.should("be.visible") // Esperar a que el mensaje se muestre
-			.should("contain", "Cart successfully updated"); // Verificar que el mensaje contenga el texto deseado
 	});
 });
 
 describe("Prueba de valor en el límite superior", () => {
-	it("El valor debe estar en 25 oprimiendo el botón '-' 24 veces, al oprimir el botón ADD TO CART mostrar mensaje exitoso", () => {
-		// Visita la página que contiene el elemento de entrada de cantidad
+	it("El valor debe estar en 25 oprimiendo el botón '-' 24 veces", () => {
 		cy.visit("http://localhost:1111/product/nike-revolution-6");
 
-		// Encuentra el elemento de entrada de cantidad por su ID
-		cy.get("#product_quantity").as("inputQuantity");
+		// Oprimir botón 'Add to cart' para agregar un producto
+		cy.get(".product-add-to-cart").click();
 
-		// Verifica que el valor inicial sea 1
+		// Abrir carrito
+		cy.contains("Cart").click();
+
+		cy.wait(500);
+
+		// Obtener campo de unidades del producto
+		cy.get(".cart-product-quantity").as("inputQuantity");
+
+		// Verifica que el valor sea 1
 		cy.get("@inputQuantity").should("have.value", "1");
 
 		// Hacer clic en el botón de aumento (incremento) de cantidad 24 veces
 		for (let i = 0; i < 24; i++) {
-			cy.get("@inputQuantity").next(".input-group-append").find(".qty-btn-plus").click();
+			cy.get(".btn-qty-add").click();
 		}
 
-		// Verificar que la cantidad sean 25 unidades
+		cy.wait(500);
+
+		// Verifica que el valor sea 2
 		cy.get("@inputQuantity").should("have.value", "25");
-
-		// Presionar botón 'Add to cart'
-		cy.get(".product-add-to-cart").click();
-
-		cy.get("#notify_message")
-			.should("be.visible") // Esperar a que el mensaje se muestre
-			.should("contain", "Cart successfully updated"); // Verificar que el mensaje contenga el texto deseado
 	});
 });
 
 describe("Prueba de valor encima del límite superior", () => {
-	it("El valor debe estar en 26 oprimiendo el botón '-' 25 veces, al oprimir el botón ADD TO CART mostrar mensaje de error", () => {
-		// Visita la página que contiene el elemento de entrada de cantidad
+	it("El valor debe estar en 25 oprimiendo el botón '-' 25 veces", () => {
 		cy.visit("http://localhost:1111/product/nike-revolution-6");
 
-		// Encuentra el elemento de entrada de cantidad por su ID
-		cy.get("#product_quantity").as("inputQuantity");
-
-		// Verifica que el valor inicial sea 1
-		cy.get("@inputQuantity").should("have.value", "1");
-
-		// Hacer clic en el botón de aumento (incremento) de cantidad 25 veces
-		for (let i = 0; i < 25; i++) {
-			cy.get("@inputQuantity").next(".input-group-append").find(".qty-btn-plus").click();
-		}
-
-		// Verificar que la cantidad sean 26 unidades
-		cy.get("@inputQuantity").should("have.value", "26");
-
-		// Presionar botón 'Add to cart'
+		// Oprimir botón 'Add to cart' para agregar un producto
 		cy.get(".product-add-to-cart").click();
 
-		cy.get("#notify_message")
-			.should("be.visible") // Esperar a que el mensaje se muestre
-			.should(
-				"contain",
-				"The quantity exceeds the max amount. Please contact us for larger orders."
-			); // Verificar que el mensaje contenga el texto deseado
+		// Abrir carrito
+		cy.contains("Cart").click();
+
+		cy.wait(500);
+
+		// Obtener campo de unidades del producto
+		cy.get(".cart-product-quantity").as("inputQuantity");
+
+		// Verifica que el valor sea 1
+		cy.get("@inputQuantity").should("have.value", "1");
+
+		// Hacer clic en el botón de aumento (incremento) de cantidad 24 veces
+		for (let i = 0; i < 25; i++) {
+			cy.get(".btn-qty-add").click();
+		}
+
+		cy.wait(500);
+
+		// Verifica que el valor sea 2
+		cy.get("@inputQuantity").should("have.value", "25");
 	});
 });
