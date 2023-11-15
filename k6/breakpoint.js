8556e1dcf2a67de09030a2b4160e212cd948bbdf
alessandrodiaz <alessdg02@gmail.com>
2023-11-14 23:14:50 -0500

    Actualizacion tests rendimiento

---
+++ k6/breakpoint.js
@@ -0,0 +1,39 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
+
+export const options = {
+	// Prueba de resistencia
+	executor: "ramping-arrival-rate", //Mantener aumento de la carga
+	stages: [
+		{ duration: "10m", target: 1000 }, // Aumentar lentamente hasta llegar a una carga enorme
+	],
+};
+
+export default () => {
+	const url = "http://localhost:1111/admin/login_action";
+
+	const payload = {
+		email: "aless@gmail.com",
+		password: "1234",
+	};
+
+	const headers = {
+		"Content-Type": "application/json",
+	};
+
+	const response = http.post(url, JSON.stringify(payload), { headers });
+
+	check(response, {
+		"status is 200": (r) => r.status === 200,
+		"login successful": (r) => JSON.parse(r.body).message === "Login successful",
+	});
+};
+
+export function handleSummary(data) {
+	return {
+		"summary-breakpoint.html": htmlReport(data),
+		stdout: textSummary(data, { indent: " ", enableColors: true }),
+	};
+}
